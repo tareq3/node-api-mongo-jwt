7decc9b214982bd354c8fe084c3a5aa4cf60a4f6
Tareq3 <mti.tareq3@gmail.com>
2018-12-18 22:55:18 +0600

    mongodb thread resolve

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
 const mongoDB = 'mongodb://localhost/node_rest_api';
-mongoose.connect(mongoDB)
+mongoose.connect(mongoDB, {
+        useNewUrlParser: true
+    })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.log('Could not connect to MongoDB...'));
 
